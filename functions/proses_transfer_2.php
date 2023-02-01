0dad65901cc13e9a1a417a514d23651933744f90
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-02-02 04:01:25 +0700

    $usertujuan

--- functions/proses_transfer_2.php
+++ functions/proses_transfer_2.php
@@ -1,16 +1,32 @@
 <?php
 function proses_transfer_2($botdata) {
-    $textsend = "<b>PROSES TRANSFER (2/4)</b>\n";
-    $textsend .= "Masukkan nominal coin yang ingin ditransfer\n";
-    $chat_id = $botdata["chat"]["id"];
-    f("bot_kirim_perintah")("sendMessage",[
-        "chat_id"=>$chat_id,
-        "text"=>$textsend,
-        "parse_mode"=>"HTML",
-        'reply_markup' => [
-            'force_reply'=>true,
-            'input_field_placeholder'=>'Tulis angka saja',
-        ],
-    ]);
+    $text = $botdata["text"] ?? "";
+    $usertujuan = f("get_user")($text);
+    if($usertujuan){
+        $textsend = "<b>PROSES TRANSFER (2/4)</b>\n";
+        $textsend .= print_r($usertujuan,true);
+        $textsend .= "Masukkan nominal coin yang ingin ditransfer\n";
+        $chat_id = $botdata["chat"]["id"];
+        f("bot_kirim_perintah")("sendMessage",[
+            "chat_id"=>$chat_id,
+            "text"=>$textsend,
+            "parse_mode"=>"HTML",
+            'reply_markup' => [
+                'force_reply'=>true,
+                'input_field_placeholder'=>'Tulis angka saja',
+            ],
+        ]);
+    }
+    else{
+        f("bot_kirim_perintah")("sendMessage",[
+            "chat_id"=>$chat_id,
+            "text"=>"Pengguna dengan ID '$text' tidak ditemukan.",
+            "parse_mode"=>"HTML",
+            'reply_markup' => [
+                'force_reply'=>true,
+                'input_field_placeholder'=>'Tulis angka saja',
+            ],
+        ]);
+    }
     return true;
 }
