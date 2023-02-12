40aaaf3b26712158a73d02fa49e58f66f2a22035
Galih Jaya Kusumah <galih.kusumah@bpk.go.id>
2023-02-12 16:45:46 +0700

    bugfix fix double send

--- functions/handle_callback_query_click_check.php
+++ functions/handle_callback_query_click_check.php
@@ -1,7 +1,7 @@
 <?php
 function handle_callback_query_click_check($botdata){
     $last_click = f("data_load")("last_click_".$botdata["message"]["chat"]["id"],0);
-    $delay_click = 3;
+    $delay_click = 5;
     if(abs(time()-$last_click) <= $delay_click){
         f("bot_kirim_perintah")('answerCallbackQuery',[
             'callback_query_id' => $botdata['id'],
--- functions/proses_transfer_acc.php
+++ functions/proses_transfer_acc.php
@@ -1,9 +1,6 @@
 <?php
 function proses_transfer_acc($botdata){
     $admacctfdata = explode("_",$botdata["data"]);
-    f("bot_kirim_perintah")('answerCallbackQuery',[
-        'callback_query_id' => $botdata['id'],
-    ]);
     $chat_id = $botdata["message"]["chat"]["id"];
     $dari_id = $admacctfdata[1];
     $dari_userdata = f("get_user")($dari_id);
@@ -16,6 +13,9 @@ function proses_transfer_acc($botdata){
 
     $dari_coin = $dari_userdata["coin"] ?? 0;
     if($dari_coin < $nominal){
+        f("bot_kirim_perintah")('answerCallbackQuery',[
+            'callback_query_id' => $botdata['id'],
+        ]);
         f("bot_kirim_perintah")("editMessageText",[
             "chat_id"=>$chat_id,
             "text"=>"❌GAGAL: Coin tidak cukup ($dari_coin)\n".$botdata["from"]["id"]." (".date("Y-m-d H:i:s").")\n\n".$botdata["message"]["text"],
@@ -29,36 +29,50 @@ function proses_transfer_acc($botdata){
         ]);
     }
     else{
-        $dari_coin -= $nominal;
-        f("db_q")("update users set coin=$dari_coin where id='$dari_id'");
-        $untuk_coin = $untuk_userdata["coin"] ?? 0;
-        $untuk_coin += $nominal;
-        f("db_q")("update users set coin=$untuk_coin where id='$untuk_id'");
-        f("bot_kirim_perintah")("editMessageText",[
-            "chat_id"=>$chat_id,
-            "text"=>"✅ Disetujui\n".$botdata["from"]["id"]." (".date("Y-m-d H:i:s").")\n\n".$botdata["message"]["text"],
-            "message_id"=>$botdata["message"]["message_id"],
-        ]);
-        f("bot_kirim_perintah")("deleteMessage",[
-            "chat_id"=>$admacctfdata[4],
-            "message_id"=>$admacctfdata[5],
-        ]);
-        $textsend = "<b>PROSES TRANSFER</b>\n✅ BERHASIL!\n";
-        $textsend .= "Dari: [$dari_id]\n($dari_nama)\n";
-        $textsend .= "Tujuan: [$untuk_id]\n($untuk_nama)\n";
-        $textsend .= "Nominal: [$nominal]\n";
-        $textsend .= "Waktu: [".date("Y-m-d H:i:s")."]\n\n/start";
-        f("bot_kirim_perintah")("sendMessage",[
-            "chat_id"=>$admacctfdata[4],
-            "text"=>$textsend,
-            "parse_mode"=>"HTML",
-        ]);
-        if(!empty($untuk_userdata['bot_active'])){
+        $last_transaction = f("data_load")("trans $dari_id to $untuk_id $nominal",0);
+        if(abs(time()-$last_transaction) <= 5 * 60){
+            f("bot_kirim_perintah")('answerCallbackQuery',[
+                'callback_query_id' => $botdata['id'],
+                'text' => "Pengiriman Koin dari $dari_id ke $untuk_id sebesar $nominal sudah dilakukan pada ".date("Y-m-d H:i:s",$last_transaction).". Tunggu 5 menit lahi jika ingin melakukan transaksi yang sama.",
+                'show_alert' => true,
+            ]);
+        }
+        else{
+            f("data_save")("trans $dari_id to $untuk_id $nominal",time());
+            f("bot_kirim_perintah")('answerCallbackQuery',[
+                'callback_query_id' => $botdata['id'],
+            ]);
+            $dari_coin -= $nominal;
+            f("db_q")("update users set coin=$dari_coin where id='$dari_id'");
+            $untuk_coin = $untuk_userdata["coin"] ?? 0;
+            $untuk_coin += $nominal;
+            f("db_q")("update users set coin=$untuk_coin where id='$untuk_id'");
+            f("bot_kirim_perintah")("editMessageText",[
+                "chat_id"=>$chat_id,
+                "text"=>"✅ Disetujui\n".$botdata["from"]["id"]." (".date("Y-m-d H:i:s").")\n\n".$botdata["message"]["text"],
+                "message_id"=>$botdata["message"]["message_id"],
+            ]);
+            f("bot_kirim_perintah")("deleteMessage",[
+                "chat_id"=>$admacctfdata[4],
+                "message_id"=>$admacctfdata[5],
+            ]);
+            $textsend = "<b>PROSES TRANSFER</b>\n✅ BERHASIL!\n";
+            $textsend .= "Dari: [$dari_id]\n($dari_nama)\n";
+            $textsend .= "Tujuan: [$untuk_id]\n($untuk_nama)\n";
+            $textsend .= "Nominal: [$nominal]\n";
+            $textsend .= "Waktu: [".date("Y-m-d H:i:s")."]\n\n/start";
             f("bot_kirim_perintah")("sendMessage",[
-                "chat_id"=>$untuk_id,
+                "chat_id"=>$admacctfdata[4],
                 "text"=>$textsend,
                 "parse_mode"=>"HTML",
             ]);
+            if(!empty($untuk_userdata['bot_active'])){
+                f("bot_kirim_perintah")("sendMessage",[
+                    "chat_id"=>$untuk_id,
+                    "text"=>$textsend,
+                    "parse_mode"=>"HTML",
+                ]);
+            }
         }
     }
 
